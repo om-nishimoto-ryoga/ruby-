3569a4c9451a8c3bed2a219e56bd302be50fc06f
nishimoto <rni07@yahoo.co.jp>
2019-04-28 18:02:35 +0900

    finidh ruby

--- ruby.rb
+++ ruby.rb
@@ -1,4 +1,15 @@
-puts "計算を始めます\n2つの数字を入力してください"
+puts "計算を始めます\n何回繰り返しますか？"
 a=gets.to_i
-b=gets.to_i
-puts "計算結果を出力します\na*b=#{a*b} \n計算を終了します"
+
+i = 1
+while i <= a do
+  puts "#{i}回目の計算\n2つの数字を入力してください"
+  b=gets.to_i
+  c=gets.to_i
+  puts "計算結果を出力します\n
+  b+c=#{b + c}\n
+  b-c=#{b - c}\n
+  b*c=#{b * c}\n
+  b/c=#{b / c}"
+  i += 1
+end
